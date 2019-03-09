d10a6bae5992306d415487e6ecee0435b43e9be8
Tyler <34544969+Exulansis0311@users.noreply.github.com>
2019-03-09 12:06:57 -0600

    Initial development of PersonModel.cs

--- DemoLibrary/PersonModel.cs
+++ DemoLibrary/PersonModel.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace DemoLibrary
 {
-    public class Class1
+    public class PersonModel
     {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
     }
 }
